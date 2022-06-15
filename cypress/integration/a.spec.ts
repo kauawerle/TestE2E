ef878c4b6db2b69a82ba6fe232e219e651474d0d
Kauã Werle <kauawerle19@gmail.com>
2022-06-15 10:03:47 -0300

    changed testes e2e

--- cypress/integration/a.spec.ts
+++ cypress/integration/a.spec.ts
@@ -1,9 +1,10 @@
-
+/// <reference types="cypress"/>
 
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/')
     cy.contains('TestsE2e')
+    expect(2).to.equal(2)
     cy.title()
     cy.wait(4000)
   })
--- cypress/integration/modal.spec.ts
+++ cypress/integration/modal.spec.ts
@@ -8,8 +8,12 @@ describe('Modal Test', () => {
     cy.contains('Cancel').click()
     cy.wait(4000)
     cy.get('button').click()
-    cy.contains('Ok').click()
     cy.wait(4000)
+    cy.contains('Ok').click()
+    cy.wait(2000)
+    cy.get('.input-test').clear().type('TESSTE');
+    cy.wait(2000)
     cy.contains('Conectar').click()
+    cy.wait(4000)
   })
 })
